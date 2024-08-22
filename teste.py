5780e3167143c8b579b95196ef19b1d2bf69c925
Yasser Abdallah <yassercondoluci@hotmail.com>
2024-08-22 11:04:22 -0400

    Removendo teste.py

--- teste.py
+++
@@ -1,8 +0,0 @@
-from rembg import remove
-from PIL import Image
-
-input_path = 'C:\\Users\\05916892195\\Pictures\\Sample Pictures\\download.jpg'
-output_path = 'C:\\Users\\05916892195\\Pictures\\Sample Pictures\\download1-removed.png'
-input = Image.open(input_path)
-output = remove(input)
-output.save(output_path)
